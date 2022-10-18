e4c02368748f8d0b1d197bc9fbcbba60d1884f29
antoniorama <92859799+tonirama@users.noreply.github.com>
2022-10-18 17:59:07 +0100

    Passos 1-4

---
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -0,0 +1,71 @@
+package com.aor.numbers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListAggregatorTest {
+
+    private List<Integer> list;
+
+    @BeforeEach
+    public void create_list() {
+        list = Arrays.asList(1,2,4,2,5);
+    }
+
+    @Test
+    public void sum() {
+        ListAggregator aggregator = new ListAggregator();
+        int sum = aggregator.sum(list);
+
+        Assertions.assertEquals(14, sum);
+    }
+
+    @Test
+    public void max() {
+        ListAggregator aggregator = new ListAggregator();
+        int max = aggregator.max(list);
+
+        Assertions.assertEquals(5, max);
+    }
+
+    @Test
+    public void max_bug_7263() {
+        List<Integer> test_list = Arrays.asList(-1,-4,-5);
+
+        ListAggregator aggregator = new ListAggregator();
+        int max = aggregator.max(test_list);
+
+        Assertions.assertEquals(-1, max);
+    }
+
+
+    @Test
+    public void min() {
+        ListAggregator aggregator = new ListAggregator();
+        int min = aggregator.min(list);
+
+        Assertions.assertEquals(1, min);
+    }
+
+    @Test
+    public void distinct() {
+        ListAggregator aggregator = new ListAggregator();
+        int distinct = aggregator.distinct(list);
+
+        Assertions.assertEquals(4, distinct);
+    }
+
+    @Test
+    public void duplicator_bug_8726() {
+        List<Integer> test_list = Arrays.asList(1,2,4,2);
+
+        ListAggregator aggregator = new ListAggregator();
+        int distinct = aggregator.distinct(test_list);
+
+        Assertions.assertEquals(3, distinct);
+    }
+}
